c9a3c4a4d568c25a4b136ad24febb56283e5cc37
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-02 11:43:51 +0300

    axios with async await

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
-import AddTutorial from '../components/AddTutorial';
-import TutorialList from '../components/TutorialList';
+import AddTutorial from "../components/AddTutorial";
+import TutorialList from "../components/TutorialList";
+import axios from "axios";
 
 const Home = () => {
+  const url = "https://cw-axios-example.herokuapp.com/api/tutorials";
+
+  const getTutorials = async () => {
+    await axios.get(url);
+  };
 
   return (
     <>
