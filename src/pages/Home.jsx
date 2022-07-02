e99403253614fa6812a323001317d18eb3b9795c
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-02 11:53:43 +0300

    useEffect added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,25 @@
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
 const Home = () => {
+  const [tutorials, setTutorials] = useState();
+
   const url = "https://cw-axios-example.herokuapp.com/api/tutorials";
 
   const getTutorials = async () => {
-    await axios.get(url);
+    //! Destructring:
+    const { data } = await axios.get(url);
+    setTutorials(data);
   };
+
+  //! Only makes requests when component mount happens
+  useEffect(() => {
+    getTutorials();
+  }, []);
+
+  // getTutorials();
 
   return (
     <>
