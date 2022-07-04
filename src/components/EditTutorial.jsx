a3c561a861523a51de08cafb4861a1c20100ee43
bemrekalkan <bemre.kalkan@gmail.com>
2022-07-04 21:40:57 +0300

    finished

--- src/components/EditTutorial.jsx
+++ src/components/EditTutorial.jsx
@@ -1,4 +1,30 @@
-const EditTutorial = () => {
+import { useState, useEffect } from "react";
+
+const EditTutorial = ({ editTutorial, editItem }) => {
+  const { id, title: newTitle, description } = editItem;
+
+  const [title, setTitle] = useState(newTitle);
+
+  const [desc, setDesc] = useState(description);
+
+  //? https://reactjs.org/docs/hooks-reference.html#usestate
+  //! Value of state variable initialState with first render Gets the initial value of the parameter. Therefore, in this case, the first value from the prop is passed to the state. Props values that are changed later are not passed to useState.If we want to pass the values from props to useState on every change, we can use the useEffect hook like componentDidUpdate.
+
+  //? ComponentDidUpdate: 👇
+  //! Local title and desc states will be updated every time newTitle or description changes 👇
+  useEffect(() => {
+    setTitle(newTitle);
+    setDesc(description);
+  }, [newTitle, description]);
+
+  const handleSave = (e) => {
+    //! 👇 turn off automatic refresh with preventDefault()
+    e.preventDefault();
+
+    editTutorial(id, title, desc);
+    setTitle("");
+    setDesc("");
+  };
   return (
     <div className="modal" tabIndex="-1" id="edit-modal">
       <div className="modal-dialog">
@@ -13,18 +39,43 @@ const EditTutorial = () => {
             />
           </div>
           <div className="modal-body">
-            <p>Modal body text goes here.</p>
+            <div className="mb-3">
+              <label htmlFor="title" className="form-label">
+                Title
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                placeholder="Enter your title"
+                value={title || ""}
+                onChange={(e) => setTitle(e.target.value)}
+                required
+              />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="desc" className="form-label">
+                Description
+              </label>
+              <input
+                type="text"
+                className="form-control"
+                id="desc"
+                placeholder="Enter your Description"
+                value={desc || ""}
+                onChange={(e) => setDesc(e.target.value)}
+                required
+              />
+            </div>
           </div>
           <div className="modal-footer">
             <button
               type="button"
-              className="btn btn-secondary"
+              className="btn btn-primary"
+              onClick={handleSave}
               data-bs-dismiss="modal"
             >
-              Close
-            </button>
-            <button type="button" className="btn btn-primary">
-              Save changes
+              Save
             </button>
           </div>
         </div>
